c3ddc1a3d2f6f962b8fb4107f3833a7e4fb68e51
akshaynikham <akshaynikhamrj@gmail.com>
2024-08-13 00:46:05 +0530

    added requirement.txt

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\PRATHIKSHA\Desktop\python\automation_and_CICD\.venv
+set VIRTUAL_ENV=C:\Users\aksha\Desktop\Projects\Automation-Pytest-Selenium-CICD\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
