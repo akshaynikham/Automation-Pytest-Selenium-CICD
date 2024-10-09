5bdebdfaad1fa0a387d692ffe056dcbd310a67f0
akshaynikham <akshaynikhamrj@gmail.com>
2024-10-09 23:57:02 +0530

    tired creating customized test discovery pattern in pytest.ini

---
+++ tests/check_base_page.py
@@ -0,0 +1,4 @@
+from pages.base_page import homepage
+
+def check_homepage(initialize_driver):
+    homepage(initialize_driver)
---
+++ tests/check_search_items.py
@@ -0,0 +1,17 @@
+import pytest
+from pages.base_page import homepage
+
+from pages.search import search_Select
+from pages.search import search_only
+
+def check_search_click_items(initialize_driver):
+    search = search_Select(initialize_driver)
+    mobile_results = search.search_items("vivo")
+    # assert len(mobile_results) > 0, "results not found"
+
+@pytest.mark.search
+@pytest.mark.parametrize("name",["vivo", "oppo", "redmi", "samsung", "iphone"])
+def check_search_items(initialize_driver,name):
+    homepage(initialize_driver)
+    search = search_only(initialize_driver)
+    results = search.search_items({name})
