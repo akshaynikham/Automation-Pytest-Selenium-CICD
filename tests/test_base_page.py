e639ff4514007b1ca327ee8780d0d2eb69f9eef2
akshaynikham <akshaynikhamrj@gmail.com>
2024-10-12 00:00:39 +0530

    integrated pytest-html reporting

--- tests/test_base_page.py
+++ tests/test_base_page.py
@@ -1,4 +1,4 @@
-from pages.base_page import homepage
-
-def test_homepage(initialize_driver):
-    homepage(initialize_driver)
+# from pages.base_page import homepage
+#
+# def test_homepage(initialize_driver):
+#     homepage(initialize_driver)
