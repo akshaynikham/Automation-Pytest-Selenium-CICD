5b346d672032be71b4f527a18f064f52efe18cba
akshaynikham <akshaynikhamrj@gmail.com>
2024-11-01 01:04:38 +0530

    updated utility file

--- pages/utils.py
+++ pages/utils.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
-import os
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
+import os
 
 def get_driver():
     selenium_url = os.getenv("SELENIUM_URL", "http://localhost:4444/wd/hub")
@@ -11,13 +10,37 @@ def get_driver():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
 
-    if selenium_url:
-        driver = webdriver.Remote(
-            command_executor=selenium_url,
-            desired_capabilities=DesiredCapabilities.CHROME,
-            options=chrome_options
-        )
-    else:
-        driver = webdriver.Chrome(options=chrome_options)
-
+    # Update `webdriver.Remote` without `desired_capabilities`
+    driver = webdriver.Remote(
+        command_executor=selenium_url,
+        options=chrome_options
+    )
     return driver
+
+
+
+
+
+# from selenium import webdriver
+# import os
+# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+# from selenium.webdriver.chrome.options import Options
+#
+# def get_driver():
+#     selenium_url = os.getenv("SELENIUM_URL", "http://localhost:4444/wd/hub")
+#
+#     chrome_options = Options()
+#     chrome_options.add_argument("--headless")
+#     chrome_options.add_argument("--no-sandbox")
+#     chrome_options.add_argument("--disable-dev-shm-usage")
+#
+#     if selenium_url:
+#         driver = webdriver.Remote(
+#             command_executor=selenium_url,
+#             desired_capabilities=DesiredCapabilities.CHROME,
+#             options=chrome_options
+#         )
+#     else:
+#         driver = webdriver.Chrome(options=chrome_options)
+#
+#     return driver
